b2d7d2fccfabf9bbfc3a0c8bc476018dad0606b9
Arfan786100 <124809236+Arfan786100@users.noreply.github.com>
2023-02-15 14:47:46 +0500

    op
    opop

---
+++ game.py
@@ -0,0 +1,42 @@
+# winning_number = 33
+# guess = 1
+# number = int(input("guess the number between 1 to 50"))
+# game_over = False
+# while not game_over:
+#     if number == winning_number:
+#         print(f"you win you guess this number in {guess} times")
+#         game_over = True
+#     else:
+#         if number < winning_number:
+#             print("too low")
+#             guess += 1
+#         number = int(input("guess agin"))
+
+                                               # my 2nd game in man names
+# winning_name = "ali"
+# guess = 1
+# name = input("enter your curect name is arfan ali usman mubishir bilal zeeshan select names ")
+# game_over = False
+# while not game_over:
+#     if name == winning_name:
+#         print(f"you win guess this name in {guess} times") 
+#         game_over = True
+#     else:
+#         if name <winning_name:
+#             print("too low")
+#             guess += 1
+#         name = input("guess agin your name")
+winning_color = "pink"
+guess = 1
+color_name = input("eter you curect color name its red,blue,green,pink,yellow and black")
+game_over = False
+while not game_over:
+    if color_name == winning_color:
+        print(f"you winning name you guess this color name{guess} times")
+        game_over = True
+    else:
+        if color_name < winning_color:
+            print("color name wright")
+            guess += 1
+        color_name = input("agin guess the color name think you")
+        # okk tree games difrent data 
